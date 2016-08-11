b9000c64aa432141a73918a9f0abcc0fbd9c4979
Mark Rivers <rivers@cars.uchicago.edu>
2016-08-11 11:19:38 -0500

    Use CAEN_DIG macro; load records

--- iocBoot/iocCaenDig/st.cmd
+++ iocBoot/iocCaenDig/st.cmd
@@ -6,9 +6,11 @@
 < envPaths
 
 ## Register all support components
-dbLoadDatabase "../../dbd/CaenDig.dbd"
+dbLoadDatabase "$(CAEN_DIG)/dbd/CaenDig.dbd"
 CaenDig_registerRecordDeviceDriver pdbbase
 
 CaenDigConfig(CD1, 0, 0, 0, 0)
+
+dbLoadRecords("$(CAEN_DIG)/db/CaenDig.template", "P=CaenDig:,R=Dig1:, PORT=CD1")
 
 iocInit
